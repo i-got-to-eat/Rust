1f608ddf882eba6bebd5053b3320ab87ecb61991
i-got-to-eat <102193600+i-got-to-eat@users.noreply.github.com>
2022-07-05 11:53:31 -0700

    Setup Modules for DataStructures

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use crate::data_structures::tree::Trie;
-
 pub mod data_structures;
 
 
+
 fn main() {
-    let tree = Trie {};
-    println!("Dis is Tree {:?}", tree);
+    let tree = Trie{};
+    println!("Trie {:?}!", tree);
 }
